0045cd2a14b63a256099736d8dca6af20305ffdf
josegilbertoguzmangutierrez <josegilbertoguzmangutierrez@outlook.com>
2024-02-24 11:16:46 -0600

    Correción de errores

--- Linea/Metodo-DDA-GUI-CTK.py
+++ Linea/Metodo-DDA-GUI-CTK.py
@@ -5,32 +5,32 @@ from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-# Set appearance mode and default color theme
+# Establecer el modo de apariencia y el tema de color predeterminado
 customtkinter.set_appearance_mode(
     "System"
-)  # Modes: "System" (standard), "Dark", "Light"
+)  # Modos: "System" (estándar), "Dark" (oscuro), "Light" (claro)
 customtkinter.set_default_color_theme(
     "green"
-)  # Themes: "blue" (standard), "green", "dark-blue"
+)  # Temas: "blue" (estándar), "green" (verde), "dark-blue" (azul oscuro)
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # Configure window
+        # Configurar la ventana
         self.title("Método DDA")
         self.geometry(f"{1100}x{580}")
         self.resizable(False, False)
 
-        # Center window on screen
+        # Centrar la ventana en la pantalla
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         x = int((screen_width - 1100) / 2)
         y = int((screen_height - 580) / 2)
         self.geometry(f"+{x}+{y}")
 
-        # Create labels and entries for X1, X2, Y1, Y2
+        # Crear etiquetas y entradas para X1, X2, Y1, Y2
         self.label_x1 = customtkinter.CTkLabel(
             master=self,
             text="X1",
@@ -41,7 +41,7 @@ class App(customtkinter.CTk):
 
         self.entry_x1 = customtkinter.CTkEntry(self, placeholder_text="")
         self.entry_x1.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
-        self.entry_x1.insert(0, "20")  # Set initial value for X1
+        self.entry_x1.insert(0, "20")  # Establecer el valor inicial para X1
 
         self.label_x2 = customtkinter.CTkLabel(
             master=self,
@@ -53,7 +53,7 @@ class App(customtkinter.CTk):
 
         self.entry_x2 = customtkinter.CTkEntry(self, placeholder_text="")
         self.entry_x2.grid(row=4, column=0, padx=20, pady=10, sticky="nsew")
-        self.entry_x2.insert(0, "60")  # Set initial value for X2
+        self.entry_x2.insert(0, "60")  # Establecer el valor inicial para X2
 
         self.label_y1 = customtkinter.CTkLabel(
             master=self,
@@ -65,7 +65,7 @@ class App(customtkinter.CTk):
 
         self.entry_y1 = customtkinter.CTkEntry(self, placeholder_text="")
         self.entry_y1.grid(row=2, column=1, padx=20, pady=10, sticky="nsew")
-        self.entry_y1.insert(0, "20")  # Set initial value for Y1
+        self.entry_y1.insert(0, "20")  # Establecer el valor inicial para Y1
 
         self.label_y2 = customtkinter.CTkLabel(
             master=self,
@@ -77,9 +77,9 @@ class App(customtkinter.CTk):
 
         self.entry_y2 = customtkinter.CTkEntry(self, placeholder_text="")
         self.entry_y2.grid(row=4, column=1, padx=20, pady=10, sticky="nsew")
-        self.entry_y2.insert(0, "50")  # Set initial value for Y2
+        self.entry_y2.insert(0, "50")  # Establecer el valor inicial para Y2
 
-        # Create "Mostrar" button
+        # Crear botón "Mostrar"
         self.button_mostrar = customtkinter.CTkButton(
             self,
             text="Mostrar",
@@ -88,7 +88,7 @@ class App(customtkinter.CTk):
         )
         self.button_mostrar.grid(row=5, column=0, padx=20, pady=10, sticky="nsew")
 
-        # Create "Limpiar" button
+        # Crear botón "Limpiar"
         self.button_limpiar = customtkinter.CTkButton(
             self,
             text="Limpiar",
@@ -97,7 +97,7 @@ class App(customtkinter.CTk):
         )
         self.button_limpiar.grid(row=5, column=1, padx=20, pady=10, sticky="nsew")
 
-        # Create table
+        # Crear tabla
         style = ttk.Style(self)
         style.configure("Treeview.Heading", font=("Arial", 16, "bold"))
         style.configure("Treeview", font=("Arial", 14))
@@ -108,35 +108,35 @@ class App(customtkinter.CTk):
             row=8, column=0, columnspan=2, padx=20, pady=10, sticky="nsew"
         )
 
-        # Create result graph
+        # Crear gráfico de resultados
         fig = Figure(figsize=(8, 6), dpi=100)
         ax = fig.add_subplot(1, 1, 1)
         canvas = FigureCanvasTkAgg(fig, master=self)
         canvas_widget = canvas.get_tk_widget()
         canvas_widget.grid(row=0, column=7, rowspan=9)
 
-    # Function to handle "Mostrar" button click event
+    # Función para manejar el evento de clic del botón "Mostrar"
     def funcion_mostrar(self):
-        # Get values from entries
+        # Obtener valores de las entradas
         X1 = float(self.entry_x1.get())
         Y1 = float(self.entry_y1.get())
         X2 = float(self.entry_x2.get())
         Y2 = float(self.entry_y2.get())
 
-        # Calculate slope
+        # Calcular la pendiente
         M = round((Y2 - Y1) / (X2 - X1), 2)
 
-        # Display slope value
-        self.label_resultado = customtkinter.CTkLabel(
+        # Mostrar el valor de la pendiente
+        self.label_resultado_pendiente = customtkinter.CTkLabel(
             master=self,
             text=f"Valor de M = {M}",
             font=customtkinter.CTkFont(size=20, weight="bold"),
         )
-        self.label_resultado.grid(
+        self.label_resultado_pendiente.grid(
             row=6, column=0, columnspan=2, padx=20, pady=0, sticky="nsew"
         )
 
-        # Determine direction
+        # Determinar la dirección
         direccion = ""
         if M == 0:
             direccion = "Línea horizontal"
@@ -152,17 +152,17 @@ class App(customtkinter.CTk):
         elif X1 > X2:
             direccion += "de derecha a izquierda"
 
-        # Display direction
-        self.label_resultado = customtkinter.CTkLabel(
+        # Mostrar la dirección
+        self.label_resultado_direccion = customtkinter.CTkLabel(
             master=self,
             text=direccion,
             font=customtkinter.CTkFont(size=20, weight="bold"),
         )
-        self.label_resultado.grid(
+        self.label_resultado_direccion.grid(
             row=7, column=0, columnspan=2, padx=20, pady=0, sticky="nsew"
         )
 
-        # Calculate coordinates using DDA algorithm
+        # Calcular las coordenadas utilizando el algoritmo DDA
         DX = abs(X1 - X2)
         DY = abs(Y1 - Y2)
         Numero_De_Pasos = int(max(DX, DY))
@@ -184,23 +184,23 @@ class App(customtkinter.CTk):
             X = X + Incremento_En_X
             Y = Y + Incremento_En_Y
 
-        # Clear the table
+        # Limpiar la tabla
         self.tabla_resultante.delete(*self.tabla_resultante.get_children())
 
-        # Insert values into the table
+        # Insertar valores en la tabla
         for i in range(Numero_De_Pasos):
             self.tabla_resultante.insert(
                 "", "end", values=(Coordenadas_En_X[i], Coordenadas_En_Y[i])
             )
 
-        # Clear the graph
+        # Limpiar el gráfico
         fig = Figure(figsize=(8, 6), dpi=100)
         ax = fig.add_subplot(1, 1, 1)
         canvas = FigureCanvasTkAgg(fig, master=self)
         canvas_widget = canvas.get_tk_widget()
         canvas_widget.grid(row=0, column=7, rowspan=9)
 
-        # Plot the line on the graph
+        # Graficar la línea en el gráfico
         ax.clear()
         ax.plot(
             [0],
@@ -231,14 +231,20 @@ class App(customtkinter.CTk):
         )
         canvas.draw()
 
-    # Function to handle "Limpiar" button click event
+    # Función para manejar el evento de clic del botón "Limpiar"
     def funcion_limpiar(self):
         self.entry_x1.delete(0, tk.END)
         self.entry_x2.delete(0, tk.END)
         self.entry_y1.delete(0, tk.END)
         self.entry_y2.delete(0, tk.END)
-        self.label_resultado.destroy()
+        self.label_resultado_pendiente.destroy()
+        self.label_resultado_direccion.destroy()
         self.tabla_resultante.delete(*self.tabla_resultante.get_children())
+        fig = Figure(figsize=(8, 6), dpi=100)
+        ax = fig.add_subplot(1, 1, 1)
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.grid(row=0, column=7, rowspan=9)
 
 
 if __name__ == "__main__":
